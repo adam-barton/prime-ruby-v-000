8dabd215ba02394e19e035c40726b6b285b9047b
Adam Barton <adam.barton333@gmail.com>
2018-05-21 02:40:37 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(num)
+  (2...num).each do |n|
+  return false if num % n == 0 || num == -Float::INFINITY..0
+  end
+    true 
+    end
+    
+  
+  
+  #  number_array.each? do |n|
+   #  n >= 1 && n % n == 0 && n % 1 == 0 
+    #end
+ 
